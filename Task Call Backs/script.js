3fa80455d87ca5769b0dc3fe6ff233da89c39573
Mujtaba <ma.mujtaba12@gmail.com>
2025-02-18 12:45:45 +0500

    Working on callbacks

---
+++ Task Call Backs/script.js
@@ -0,0 +1,17 @@
+// console.log('Before setTimeout');
+// fetchUser(1, function(user) {
+//     console.log('User:', user);
+// });
+// console.log('After setTimeout');
+// function fetchUser(id, callback) {
+//     setTimeout(() => {
+//         console.log('Fetching user from database');
+//         callback({ id: id, name: 'John' });
+//     }, 1000);
+// }
+
+// // Output:
+// // Before setTimeout
+// // After setTimeout
+// // Fetching user from database
+// // User: { id: 1, name: 'John' }
